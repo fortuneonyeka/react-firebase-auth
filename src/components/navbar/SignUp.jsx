d7ebd9e99c5f4d3737102fc923adfc8f0f5afc6c
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-19 20:37:24 +0100

    setup env loval variable

--- src/components/navbar/SignUp.jsx
+++ src/components/navbar/SignUp.jsx
@@ -2,55 +2,55 @@ import React,{useRef, useState} from 'react'
 import {signup,login, logout, useAuth} from "../../firebase"
 
 const SignUp = () => {
-  const [loading, setLoading] = useState(false)
-  const currentUser = useAuth
+  // const [loading, setLoading] = useState(false)
+  // const currentUser = useAuth
 
-  const emailRef = useRef();
-  const passwordRef = useRef();
+  // const emailRef = useRef();
+  // const passwordRef = useRef();
 
 
-  async function handleSignup() {
-    setLoading(true)
-    try {
-      await signup(emailRef.current.value, passwordRef.current.value)
-    } catch {
-      alert("Error")
-    }
-    setLoading(false)
-  }
+  // async function handleSignup() {
+  //   setLoading(true)
+  //   try {
+  //     await signup(emailRef.current.value, passwordRef.current.value)
+  //   } catch {
+  //     alert("Error")
+  //   }
+  //   setLoading(false)
+  // }
 
-  async function handleLogin() {
-    setLoading(true)
-    try {
-      await login(emailRef.current.value, passwordRef.current.value)
-    } catch {
-      alert("Error!")
-    }
-    setLoading(false)
-  }
+  // async function handleLogin() {
+  //   setLoading(true)
+  //   try {
+  //     await login(emailRef.current.value, passwordRef.current.value)
+  //   } catch {
+  //     alert("Error!")
+  //   }
+  //   setLoading(false)
+  // }
 
-  async function handleLogout() {
-    setLoading(true)
-    try {
-      await logout();
-    } catch {
-      alert("Error!")
-    }
-   setLoading(false)
-  }
+  // async function handleLogout() {
+  //   setLoading(true)
+  //   try {
+  //     await logout();
+  //   } catch {
+  //     alert("Error!")
+  //   }
+  //  setLoading(false)
+  // }
 
   return (
     <div className="col-4 pt-5">
 
       <h3 className="d-flex justify-content-center">Sign Up</h3>
-      <div>Cuurently Signed In As: {currentUser?.email}</div>
+      <div>Curently Signed In As:</div>
       <div className="form-group">
         <label>Email</label>
-        <input ref={emailRef} type="email" className="form-control" placeholder="Email@"/>
+        <input type="email" className="form-control" placeholder="Email@"/>
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input ref={passwordRef}
+        <input 
           type="password"
           className="form-control"
           placeholder="Password"
@@ -61,14 +61,10 @@ const SignUp = () => {
      
 
       <div className="d-flex justify-content-center mt-4">
-        <button  onClick={handleSignup}  className="btn btn-primary btn-lg btn-block px-5 ">
+        <button   className="btn btn-primary btn-lg btn-block px-5 ">
           Sign Up
         </button>
-        <button  onClick={handleLogin}  className="btn btn-primary btn-lg btn-block px-5 ">
-          Login
-        </button>
-
-        <button disabled={loading || currentUser == null} onClick={handleLogout} className="btn btn-primary btn-lg btn-block px-5 mx-3">Log Out</button>
+       
       </div>
     </div>
   )
