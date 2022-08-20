fe0c2c392c85b8e36d4a7c8a95221e81fa4b1acd
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-20 08:05:47 +0100

    update sign up form styling

--- src/components/navbar/SignUp.jsx
+++ src/components/navbar/SignUp.jsx
@@ -1,72 +1,47 @@
 import React,{useRef, useState} from 'react'
+import {Form, Card, Button, Container} from "react-bootstrap"
 import {signup,login, logout, useAuth} from "../../firebase"
 
 const SignUp = () => {
-  // const [loading, setLoading] = useState(false)
-  // const currentUser = useAuth
-
-  // const emailRef = useRef();
-  // const passwordRef = useRef();
-
-
-  // async function handleSignup() {
-  //   setLoading(true)
-  //   try {
-  //     await signup(emailRef.current.value, passwordRef.current.value)
-  //   } catch {
-  //     alert("Error")
-  //   }
-  //   setLoading(false)
-  // }
-
-  // async function handleLogin() {
-  //   setLoading(true)
-  //   try {
-  //     await login(emailRef.current.value, passwordRef.current.value)
-  //   } catch {
-  //     alert("Error!")
-  //   }
-  //   setLoading(false)
-  // }
-
-  // async function handleLogout() {
-  //   setLoading(true)
-  //   try {
-  //     await logout();
-  //   } catch {
-  //     alert("Error!")
-  //   }
-  //  setLoading(false)
-  // }
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
 
   return (
-    <div className="col-4 pt-5">
-
-      <h3 className="d-flex justify-content-center">Sign Up</h3>
-      <div>Curently Signed In As:</div>
-      <div className="form-group">
-        <label>Email</label>
-        <input type="email" className="form-control" placeholder="Email@"/>
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input 
-          type="password"
-          className="form-control"
-          placeholder="Password"
-          required
-          
-        />
-      </div>
+    <Container className="d-flex align-items-center justify-content-center "
+    style={{minHeight:"100vh"}}
+    >
+      <div className="w-100 " style={{maxWidth:"400px"}}>
+    <Card className="sign-up">
+      <Card.Body>
+        <h2 className="text-center mb-4">Sign Up</h2>
+        <Form>
+          <Form.Group id="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" ref={emailRef} required/>
+          </Form.Group>
+          <Form.Group id="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" ref={passwordRef} required/>
+          </Form.Group>
+          <Form.Group id="password-confirm">
+            <Form.Label>Password Confirmation</Form.Label>
+            <Form.Control type="password" ref={passwordConfirmRef} required/>
+          </Form.Group>
+          <Button className="w-100 mt-4 py-3" type="submit">Sign Up</Button>
+        </Form>
+      </Card.Body>
+    </Card>
+   
+   <div className="w-100 text-center mt-2 ">
+      Already have an account? Log in
+   </div>
+   </div>
      
+    </Container>
+    
 
-      <div className="d-flex justify-content-center mt-4">
-        <button   className="btn btn-primary btn-lg btn-block px-5 ">
-          Sign Up
-        </button>
-       
-      </div>
-    </div>
+      
   )
 }
 
