b0104bd672430927fe60a668ea97fb4f05eb5c34
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-21 16:04:38 +0100

    recreate firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,11 @@
-import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+import { useState, useEffect } from "react";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword } from "firebase/auth";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
-const firebaseConfig = {
+
+ const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APA_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -11,6 +15,30 @@ const firebaseConfig = {
 
 };
 
-export const auth = getAuth
+ export const auth = getAuth
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+
+export function signup (email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+   };
+   export function login (email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+   };
+  
+   export function logout() {
+   return signOut(auth);
+   }
+  
+  // //custom react Hook
+   export function useAuth() {
+     const [currentUser, setCurrentUser] = useState();
+  
+     useEffect(() => {
+     const unsub =  onAuthStateChanged(auth, user => setCurrentUser(user));
+     return unsub;
+    },[])
+  
+     return currentUser;
+   }
