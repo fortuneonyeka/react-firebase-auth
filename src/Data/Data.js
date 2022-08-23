4201a31488dda1d0787499ac29bf683c42c5e1c0
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-23 10:03:45 +0100

    created sample data

---
+++ src/Data/Data.js
@@ -0,0 +1,10 @@
+const Data = [
+  {
+    name: "About",
+    about: "Retink is an AI-driven content creation and management platform that enables small businesses to create customised marketing content with a particular focus to brand tone at a budget, saving them time and energy, which is channeled to building and scaling the business instead.",
+    services: "Easily create, manage, access, and publish content online.Create unlimited content forever.Choose content, set budget, approve the project, and drop a review.",
+
+  },
+]
+
+export default Data
